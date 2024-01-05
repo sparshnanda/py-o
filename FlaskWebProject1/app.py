3b1bc6e8d0c48fdfbdb1b2723631cf94a19142bb
Sparsh Nanda <sparshnanda9@gmail.com>
2024-01-05 19:13:20 +0530

    commit

--- FlaskWebProject1/app.py
+++ FlaskWebProject1/app.py
@@ -63,7 +63,7 @@ def process():
 
 def run():
     if __name__ == '__main__':
-        app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0', port=5000)
 
 # ... (Your route handlers and other functions)
 
